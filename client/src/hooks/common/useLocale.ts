32130965016598eaa9b15cd5b0910d8a88aff480
Redian Fikri <redianmf@gmail.com>
2024-07-25 09:34:00 +0700

    fix(client): handle invalid selected language in localstorage

--- client/src/hooks/common/useLocale.ts
+++ client/src/hooks/common/useLocale.ts
@@ -1,14 +1,42 @@
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { DefaultLang } from "../../locales";
+
+import { DefaultLang, LanguageOptions } from "../../locales";
+
+const availableLang = LanguageOptions.map((item) => item.lang);
 
 const useLocale = () => {
   const { i18n } = useTranslation();
-  const currentLang: string = localStorage.getItem("lang") || DefaultLang;
+  const [currentLang, setCurrentLang] = useState<string>("");
+  const currLang = localStorage.getItem("lang");
+  // const currentLang: string = localStorage.getItem("lang") || DefaultLang;
+
+  const getCurrentLang = () => {
+    // No selected language in localstorage
+    if (!currLang) {
+      handleChangeLang(DefaultLang);
+      setCurrentLang(DefaultLang);
+      return;
+    }
+
+    // Selected language in localstorage is invalid
+    if (!availableLang.includes(currLang)) {
+      handleChangeLang(DefaultLang);
+      setCurrentLang(DefaultLang);
+      return;
+    }
+
+    setCurrentLang(currLang);
+  };
 
   const handleChangeLang = (lang: string) => {
     localStorage.setItem("lang", lang);
     i18n.changeLanguage(lang);
   };
+
+  useEffect(() => {
+    getCurrentLang();
+  }, [currLang]);
 
   return { currentLang, handleChangeLang };
 };
