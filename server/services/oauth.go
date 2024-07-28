32f834b2034af4c12b65bc3a5fe0ee0805157175
Redian Fikri <redianmf@gmail.com>
2024-07-28 21:26:44 +0700

    feat(server): implement get profile data from google

---
+++ server/services/oauth.go
@@ -0,0 +1,23 @@
+package services
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func GetGoogleUserInfo(accessToken string) (map[string]interface{}, error) {
+	userInfoEndpoint := "https://www.googleapis.com/oauth2/v2/userinfo"
+	resp, err := http.Get(fmt.Sprintf("%s?access_token=%s", userInfoEndpoint, accessToken))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var userInfo map[string]interface{}
+	if err := json.NewDecoder(resp.Body).Decode(&userInfo); err != nil {
+		return nil, err
+	}
+
+	return userInfo, nil
+}
